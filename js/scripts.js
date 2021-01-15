0503613611fc1f264becbb349447a5dad8e07815
Nikhil Panickssery <nikhil.panickssery@gmail.com>
2021-01-15 09:28:06 -0500

    sale framework, theme selection framework

--- js/scripts.js
+++ js/scripts.js
@@ -67,3 +67,50 @@
   
   })(jQuery); // End of use strict
   
+// Get the button
+var boomButton = document.getElementById("boom-button");
+var smashButton = document.getElementById("smash-button");
+var crashedButton = document.getElementById("crashed-button");
+
+// Get the modal
+var boomModal = document.getElementById("boom-modal");
+var smashModal = document.getElementById("smash-modal");
+var crashedModal = document.getElementById("crashed-modal");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+
+// When the user clicks the button, open the modal 
+boomButton.onclick = function() {
+  boomModal.style.display = "block";
+}
+smashButton.onclick = function() {
+    smashModal.style.display = "block";
+}
+crashedButton.onclick = function() {
+    crashedModal.style.display = "block";
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  boomModal.style.display = "none";
+}
+span2.onclick = function() {
+  smashModal.style.display = "none";
+}
+span3.onclick = function() {
+  crashedModal.style.display = "none";
+}
+//   crashedModal.style.display = "none";
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == boomModal || event.target == smashModal || event.target == crashedModal) {
+    boomModal.style.display = "none";
+    smashModal.style.display = "none";
+    crashedModal.style.display = "none";
+  }
+}
